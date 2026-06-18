38864f8cd52328066aecd4ee2c36beee920fcb4d
Raj Rathod <rajsnehrathod@gmail.com>
2026-06-19 10:46:32 +1200

    Moved Docker Compose configuration to environment variables

--- app/main.py
+++ app/main.py
@@ -4,19 +4,29 @@ from app.database.db import engine, Base
 from app.models.employee import Employee
 from app.routers.employee_routes import router as employee_router
 
+# Create database tables
 Base.metadata.create_all(bind=engine)
 
+# FastAPI app
 app = FastAPI(
     title="Employee Management API",
     description="FastAPI backend connected to PostgreSQL using SQLAlchemy",
     version="1.0.0"
 )
 
+# Routers
 app.include_router(employee_router)
 
-
+# Root endpoint
 @app.get("/")
-def health_check():
+def root():
     return {
         "message": "Employee Management API is running"
+    }
+
+# Health endpoint
+@app.get("/health")
+def health_check():
+    return {
+        "status": "healthy"
     }
